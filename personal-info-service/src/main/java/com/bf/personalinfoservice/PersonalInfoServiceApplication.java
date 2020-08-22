df0399e54a0a048b895865ab6431133d962eeb0c
Sephiroth <hljyy96@126.com>
2020-08-22 15:01:56 -0400

    info

---
+++ personal-info-service/src/main/java/com/bf/personalinfoservice/PersonalInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bf.personalinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalInfoServiceApplication.class, args);
+    }
+
+}
