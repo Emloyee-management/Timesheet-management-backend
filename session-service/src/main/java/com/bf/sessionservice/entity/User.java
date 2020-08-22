8494e0519c21b6f718894e637278d4720240e399
Zishan Liu <zisaanlau@gmail.com>
2020-08-21 22:33:25 -0400

    add token

--- session-service/src/main/java/com/bf/sessionservice/entity/User.java
+++ session-service/src/main/java/com/bf/sessionservice/entity/User.java
@@ -29,6 +29,16 @@ public class User {
 
     private String emergency2Phone;
 
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     public String getId() {
         return id;
     }
