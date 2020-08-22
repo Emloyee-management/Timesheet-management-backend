2c88d814f60e4d42e2340d0f3ab5f88cdb914341
Zishan Liu <zisaanlau@gmail.com>
2020-08-21 23:46:36 -0400

    small change

--- view-time-sheet-service/src/main/java/com/bf/viewtimesheetservice/controller/TimesheetController.java
+++ view-time-sheet-service/src/main/java/com/bf/viewtimesheetservice/controller/TimesheetController.java
@@ -5,11 +5,13 @@ import com.bf.viewtimesheetservice.service.TimesheetService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@RestController
 public class TimesheetController {
 
     @Resource
