0a71b21d5046f2083034fd1aef5460a958a86c10
Sephiroth <hljyy96@126.com>
2020-08-22 21:21:10 -0400

    eureka

---
+++ discovery-server/src/main/java/com/bf/discoveryserver/config/Eureka.java
@@ -0,0 +1,22 @@
+//package com.bf.discoveryserver.config;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//
+///**
+// * @description:
+// * @author: Yang Yuan
+// * @Time: 2020/8/22
+// */
+//
+//@Configuration
+//@EnableWebSecurity
+//public class Eureka extends WebSecurityConfigurerAdapter{
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        super.configure(http);
+//    }
+//}
