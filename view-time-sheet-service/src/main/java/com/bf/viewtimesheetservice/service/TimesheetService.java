b348ea56dddfab6ff901f620a83786b7cf3c7070
Sephiroth <hljyy96@126.com>
2020-08-24 22:19:46 -0400

    get time sheet by id

--- view-time-sheet-service/src/main/java/com/bf/viewtimesheetservice/service/TimesheetService.java
+++ view-time-sheet-service/src/main/java/com/bf/viewtimesheetservice/service/TimesheetService.java
@@ -2,8 +2,12 @@ package com.bf.viewtimesheetservice.service;
 
 import com.bf.viewtimesheetservice.entity.Timesheet;
 import com.bf.viewtimesheetservice.repository.TimesheetRepository;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,6 +19,9 @@ public class TimesheetService {
     TimesheetRepository timesheetRepository;
 
     @Resource
+    MongoTemplate template;
+
+    @Resource
     public void setSessionRepository(TimesheetRepository tr) {
         this.timesheetRepository = tr;
     }
@@ -24,7 +31,10 @@ public class TimesheetService {
     }
 
     public Timesheet findTimeSheetBy_Id(String id) {
-        return timesheetRepository.findByShabiId(id);
+        ObjectId oid = new ObjectId(id);
+//        System.out.println(oid.getClass() + oid.toString());
+        Query query = new Query(Criteria.where("_id").is(oid));
+        return template.find(query, Timesheet.class).get(0);
     }
 
     public List<Timesheet> findAllTimeSheet() {
@@ -40,5 +50,13 @@ public class TimesheetService {
         }
         return true;
     }
+//    Timesheet findById(String id) {
+//        BasicDBObject query = new BasicDBObject();
+//        query.put("_id", new ObjectId(id));
+//        DBCollection users = db.getCollection(tableName);
+//
+//        Timesheet res = timesheetRepository.findOne(query);
+//        return res;
+//    }
 
 }
